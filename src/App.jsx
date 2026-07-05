0daa329debde89b2bccaaf4d3907c6368d359cb6
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-05 15:20:28 -0300

    add comentarios e os estagios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+// CSS
 import "./App.css";
+
+// react
+import { useCallback, useEffect, useState } from "react";
+
+// dados
+import { wordsList } from "./data/words";
+
+// components
 import StartScreen from "./components/StartScreen";
 
+const stage = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 export default function App() {
+
+  
   return (
     <div className="App">
       <StartScreen />
