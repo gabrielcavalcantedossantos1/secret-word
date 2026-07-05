3c8d635af9a508e451472ee803587c51468e012b
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-05 16:05:12 -0300

    adicionando a rota dinamica

--- src/pages/StartScreen/index.jsx
+++ src/pages/StartScreen/index.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import "./style.css";
 
 export default function StartScreen() {
+  const navigate = useNavigate();
   return (
     <div className="start">
       <h1>Palavra Secreta</h1>
 
       <p>Clique no botão abaixo para começar a jogar</p>
-      <button>Começar o jogo</button>
+      <button onClick={() => navigate("/game")}>Começar o jogo</button>
     </div>
   );
 }
