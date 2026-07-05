9ae14dc3169048899ee8b7d01b11222130653398
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-05 15:38:49 -0300

    excluindo arquivos desnecessarios

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,14 @@ import { useCallback, useEffect, useState } from "react";
 import { wordsList } from "./data/words";
 
 // components
-import StartScreen from "./components/StartScreen";
+import StartScreen from "./pages/StartScreen";
 
-const stage = [
-  { id: 1, name: "start" },
-  { id: 2, name: "game" },
-  { id: 3, name: "end" },
-];
 
 export default function App() {
-  const [gameStage, setGameStage] = useState(stages[0].name);
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen />}
-      {gameStage === "game" && <StartScreen />}
-      {gameStage === "end" && <StartScreen />}
+      
     </div>
   );
 }
