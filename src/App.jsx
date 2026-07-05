0983da37b91b6e1be2001a173b6f2898920fd016
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-05 14:59:53 -0300

    implementando a primeira pagina a jogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import StartScreen from "./components/StartScreen";
 
 export default function App() {
   return (
     <div className="App">
       <h2>Secret Words</h2>
+      <StartScreen />
     </div>
   );
 }
