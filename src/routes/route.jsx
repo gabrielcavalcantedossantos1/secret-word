6ab4288f503c2e8b7400f84b61daba46758de718
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-05 16:08:30 -0300

    rota do jogo

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -2,12 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 
 import StartScreen from "../pages/StartScreen/index";
 import Layout from "../components/layout/layout";
+import Game from "../pages/game";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <StartScreen /> }],
+    children: [
+      { index: true, element: <StartScreen /> },
+      { path: "/game", element: <Game /> },
+    ],
   },
 ]);
 
