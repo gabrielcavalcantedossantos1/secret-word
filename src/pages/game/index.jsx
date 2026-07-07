fdb8eabbbe379da7ad72896a82b63ed04d6c391b
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-06 22:00:07 -0300

    finalizando a estrutura do JSX da pagina Game.jsx

--- src/pages/game/index.jsx
+++ src/pages/game/index.jsx
@@ -41,10 +41,30 @@ export default function Game() {
     navigate("/end");
   }
   return (
-    <div>
-      <h1>game</h1>
-
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra: </h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>{pickedCategory}</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar a palavra!</p>
+        <form>
+          <input type="text" name="letter" maxLength={1} required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já digitadas:</p>
+        <span>a, </span>
+        <span>b</span>
+      </div>
     </div>
   );
 }
