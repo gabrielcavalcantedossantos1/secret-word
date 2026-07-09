2bd5b047d4b1bfb1359556cf8aeccad227d00a4e
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-08 22:33:39 -0300

    importando estados que irei precisar

--- src/pages/gameOver/index.jsx
+++ src/pages/gameOver/index.jsx
@@ -1,11 +1,19 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./style.css";
+import { useEffect } from "react";
 
 export default function GameOver() {
   const navigate = useNavigate();
 
+  const {state} = useLocation();
+
+  const score = state?.score || 0;
+  const pickedWord = state?.pickedWord || "";
+
   function retry() {
     navigate("/game");
+
+    
   }
   return (
     <div>
