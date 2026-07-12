5d6ed66418d2763a1639aabd7c147f0ed5092dd8
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-12 12:26:12 -0300

    finalizando o estilo da pagina gameOver

--- src/pages/gameOver/index.jsx
+++ src/pages/gameOver/index.jsx
@@ -5,18 +5,25 @@ import { useEffect } from "react";
 export default function GameOver() {
   const navigate = useNavigate();
 
-  const {state} = useLocation();
+  const { state } = useLocation();
 
   const score = state?.score || 0;
   const pickedWord = state?.pickedWord || "";
 
   function retry() {
     navigate("/game");
-
-    
   }
   return (
-    <div>
+    <div className="game-over">
+      <h1>Você perdeu!</h1>
+      <h4>
+        A palavra era: <span>{pickedWord}</span>
+      </h4>
+
+      <h4>
+        Pontuação: <span>{score} </span>
+      </h4>
+      <p>Deseja recomeçar?</p>
       <button onClick={retry}>Recomeçar</button>
     </div>
   );
